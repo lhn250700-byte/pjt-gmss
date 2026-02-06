eeca9d9199e671c8146713250f77449925c2d8c6
lhn250700-byte <lhn250700@gmail.com>
2026-02-06 18:13:07 +0900

    feat: popular post list and score

---
+++ backend/src/main/java/com/study/spring/Bbs/controller/BbsController.java
@@ -0,0 +1,41 @@
+package com.study.spring.Bbs.controller;
+
+import com.study.spring.Bbs.dto.PopularPostClassDto;
+import com.study.spring.Bbs.dto.PopularPostDto;
+import com.study.spring.Bbs.service.BbsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class BbsController {
+    @Autowired
+    BbsService bbsService;
+
+    // [실시간 인기글]
+    @GetMapping("/posts/popular/realtime")
+    public List<PopularPostClassDto> getRealtimePopularPosts (){
+        return bbsService.findRealtimePopularPosts();
+    }
+
+    // [주간 인기글]
+    @GetMapping("/posts/popular/weekly")
+    public List<PopularPostDto> getWeeklyPopularPosts (){
+        return bbsService.findWeeklyPopularPosts();
+    }
+
+    // [월간 인기글]
+    @GetMapping("/posts/popular/monthly")
+    public void getMonthlyPopularPosts (){
+
+    }
+
+    // [추천순]
+    @GetMapping("/posts/recommendation")
+    public void getRecommendedPosts (){
+
+    }
+
+}
--- backend/src/main/java/com/study/spring/Member/controller/KakaoTestController.java
+++ backend/src/main/java/com/study/spring/Member/controller/KakaoTestController.java
@@ -1,135 +1,50 @@
 package com.study.spring.Member.controller;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
+import com.study.spring.Member.service.KakaoService;
+import jakarta.validation.constraints.Email;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-@Controller
+@Log4j2
+@RestController
 public class KakaoTestController {
+    @Autowired
+    KakaoService kakaoService;
+
     @GetMapping("/kakao/auth-code") // 카카오에서 설정한 주소(redirect-uri)
-    public ResponseEntity<String> kakaoCallback(
-            @RequestParam(required = false) String code,
-            @RequestParam(required = false) String error,
-            @RequestParam String state
-    ) {
-        // 1. 인가 코드 로그
-        System.out.println("인가 코드: " + code);
-        System.out.println("이메일: " + state  );
-
-        // 2. 인가 코드 → 토큰
-        KakaoTokenResponse token = getAccessToken(code);
-        System.out.println("access_token: " + token.accessToken());
-
-        // 3. 토큰 → 사용자 정보
-        KakaoUserInfoResponse userInfo =
-                getUserInfo(token.accessToken());
-
-        System.out.println("kakaoId: " + userInfo.id());
-        System.out.println("nickname: " +
-                userInfo.kakaoAccount().profile().nickname());
-
-        return ResponseEntity.ok("카카오 로그인 성공");
-    }
-
-    private KakaoTokenResponse getAccessToken(String code) {
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
-
-        MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
-        body.add("grant_type", "authorization_code");
-        body.add("client_id", "db84a36a76acabde3b54a4323ecec380");
-        body.add("redirect_uri", "http://localhost:8080/kakao/auth-code");
-        body.add("code", code);
-        body.add("client_secret", "NcLc5uPCtDjEA8dixgMsm3UntN6dywp2");
-
-        HttpEntity<MultiValueMap<String, String>> request =
-                new HttpEntity<>(body, headers);
-
-        RestTemplate restTemplate = new RestTemplate();
-
-        ResponseEntity<KakaoTokenResponse> response =
-                restTemplate.postForEntity(
-                        "https://kauth.kakao.com/oauth/token",
-                        request,
-                        KakaoTokenResponse.class
-                );
-
-        return response.getBody();
-    }
-
-    private KakaoUserInfoResponse getUserInfo(String accessToken) {
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("Authorization", "Bearer " + accessToken);
-
-        HttpEntity<Void> request = new HttpEntity<>(headers);
-
-        RestTemplate restTemplate = new RestTemplate();
-
-        ResponseEntity<KakaoUserInfoResponse> response =
-                restTemplate.exchange(
-                        "https://kapi.kakao.com/v2/user/me",
-                        HttpMethod.GET,
-                        request,
-                        KakaoUserInfoResponse.class
-                );
-
-        return response.getBody();
-    }
-
-    public record KakaoTokenResponse(
-
-            @JsonProperty("access_token")
-            String accessToken,
-
-            @JsonProperty("token_type")
-            String tokenType,
-
-            @JsonProperty("refresh_token")
-            String refreshToken,
-
-            @JsonProperty("expires_in")
-            Long expiresIn,
-
-            @JsonProperty("scope")
-            String scope
-    ) {}
-
-    public record KakaoUserInfoResponse(
-
-            @JsonProperty("id")
-            Long id,
-
-            @JsonProperty("connected_at")
-            String connectedAt,
-
-            @JsonProperty("kakao_account")
-            KakaoAccount kakaoAccount
+    public ResponseEntity<String> kakaoCallback(@RequestParam(required = false) String code,
+                                                @RequestParam(required = false) String error, // 인증 실패 시 에러 코드
+                                                @RequestParam(required = false) String error_description // 인증 실패 시 에러 메시지
     ) {
 
-        public record KakaoAccount(
-
-                @JsonProperty("profile")
-                Profile profile
-        ) {
-            public record Profile(
-
-                    @JsonProperty("nickname")
-                    String nickname,
-
-                    @JsonProperty("profile_image_url")
-                    String profileImageUrl
-            ) {}
+        // 인증 실패
+        if (error != null) {
+            log.info("카카오 로그인 실패 " + error + " / " + error_description);
+            return ResponseEntity.badRequest().body("카카오 로그인 실패 " + error_description);
         }
-    }
 
+        // 인가 코드 없을 때
+        if (code == null || code.isEmpty()) {
+            log.info("인가 코드가 없습니다.");
+            return ResponseEntity.badRequest().body("인가 코드가 존재하지 않습니다.");
+        }
+
+        try {
+            kakaoService.kakaoLogin(code);
+            return ResponseEntity.ok("카카오 로그인 성공");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("카카오 로그인 처리 중 오류 발생 " + e.getMessage());
+        }
+
+    }
 }
